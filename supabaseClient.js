00ac68b83bb4c4b61ae8ff3057a9100e0329b9b3
Ulices-byte <159735603+Juamaica@users.noreply.github.com>
2026-08-21 14:54:47 -0400

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -2,9 +2,7 @@
 //  CODYWEB.COM — supabaseClient.js
 //  Conexión al proyecto de Supabase
 // ============================================================
-
-const SUPABASE_URL = 'sb_publishable_EDtHyo5q9g0lFVHf6S2uew_wC8Ndkip';
+const SUPABASE_URL = 'https://ovpruwjtlvphrxvgbavm.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im92cHJ1d2p0bHZwaHJ4dmdiYXZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODczMzQwNjQsImV4cCI6MjEwMjkxMDA2NH0.0xPF7gl9j1Ivm4RRCGf2Se4HTJ_peVDlu3XKDBnVmJY';
-
 // 'supabase' aquí es la librería cargada desde el CDN (ver index.html/login.html)
 const sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
